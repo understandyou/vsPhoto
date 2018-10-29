d13f8398129e19751149bc5c2b6014ff4ce0611a
trytheworld <trytheworld@DESKTOP-VQ2VT8Q>
2018-10-29 14:55:53 +0800

    首次提交

---
+++ layui/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using layuiBLL;
+
+namespace layui.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ts1() {
+            return View();
+        }
+        public ActionResult ts2()
+        {
+            return View();
+        }
+        public ActionResult btupload() {
+
+            return View();
+        }
+
+        public ActionResult upload(HttpPostedFileBase[] file)
+        {
+            //string result = "";
+            Dictionary<string, object> map = new Dictionary<string, object>();
+            try
+            {
+                if (file != null)
+                {
+                    var path = Server.MapPath(string.Format("~/{0}", "File"));
+                    mydir.createdir(path);
+                    for (int i = 0; i < file.Length; i++)
+                    {
+                        var name = file[i].FileName;
+                        file[i].SaveAs(Path.Combine(path, name));
+                    }
+                }
+                map.Add("result", "ok");
+            }
+            catch (Exception e)
+            {
+                map.Add("result", "no");
+
+            }
+            return Json(new {
+                //result = "{'result':'ok'}"
+                result = map
+            });
+        }
+
+    }
+}
---
+++ layuiBLL/mydir.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace layuiBLL
+{
+    public class mydir
+    {
+        public static void createdir(string path) {
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+        }
+    }
+}
